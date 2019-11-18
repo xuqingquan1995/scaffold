e89fa4f30e47d03e182399a33251deb08c8a3915
许清泉 <xuqingquan1995@gmail.com>
2019-11-18 12:44:13 +0800

    添加获取ssid方法 更新依赖版本

--- sample/src/main/java/top/xuqingquan/sample/MainActivity.kt
+++ sample/src/main/java/top/xuqingquan/sample/MainActivity.kt
@@ -31,77 +31,77 @@ class MainActivity : SimpleActivity() {
 
     override fun getLayoutId() = R.layout.activity_main
 
-    override fun initData(savedInstanceState: Bundle?) {
-        launch {
-            toast("X5初始化中")
-            Timber.d("WebConfig.hasX5()===${WebConfig.hasX5()}")
-            var i = 0
-            while (!WebConfig.hasX5() && i < 60) {
-                delay(1000)
-                i++
-                Timber.d("WebConfig.hasX5()===${WebConfig.hasX5()}")
-            }
-            toast("X5初始化---${WebConfig.hasX5()}")
-            startActivity<HtmlPrinterActivity>()
-            finish()
-        }
-    }
-
-    //    override fun initData(savedInstanceState: Bundle?) {
-//        val factory = BeanDataSourceFactory()
-//        val listing = Listing(
-//            pagedList = factory.toLiveData(config),
-//            networkState = Transformations.switchMap(factory.sourceLiveData) { it.networkState },
-//            refreshState = Transformations.switchMap(factory.sourceLiveData) { it.initialLoad },
-//            empty = Transformations.switchMap(factory.sourceLiveData) { it.empty },
-//            refresh = { factory.sourceLiveData.value?.invalidate() },
-//            retry = { factory.sourceLiveData.value?.retryAllFailed() },
-//            exception = Transformations.switchMap(factory.sourceLiveData) { it.exception }
-//        )
-//        adapter = BeanAdapter {
-//            listing.retry.invoke()
-//        }
-//        list.adapter = adapter
-//        listing.pagedList.observe(this, Observer {
-//            adapter.submitList(it)
-//            Timber.d("adapter.currentList?.size===>${adapter.currentList?.size}")
-//            adapter.currentList?.forEach { sub ->
-//                Timber.d("adapter.currentList$sub")
+//    override fun initData(savedInstanceState: Bundle?) {
+//        launch {
+//            toast("X5初始化中")
+//            Timber.d("WebConfig.hasX5()===${WebConfig.hasX5()}")
+//            var i = 0
+//            while (!WebConfig.hasX5() && i < 60) {
+//                delay(1000)
+//                i++
+//                Timber.d("WebConfig.hasX5()===${WebConfig.hasX5()}")
 //            }
-//        })
-//        listing.empty.observe(this, Observer {
-//            toast("empty--$it")
-//        })
-//        listing.exception.observe(this, Observer {
-//            toast("exception${it.message}")
-//        })
-//        listing.networkState.observe(this, Observer {
-//            adapter.setNetworkState(it)
-//        })
-//        listing.refreshState.observe(this, Observer {
-//            Timber.d("refreshState===$it")
-//            swipe_refresh.isRefreshing = it == NetworkStatus.RUNNING
-//        })
-//        swipe_refresh.setOnRefreshListener {
-//            Timber.d("refreshState===1111")
-//            listing.refresh.invoke()
-//        }
-//        adapter.listener = object : SimplePagedListAdapter.OnViewClickListener<Subjects>() {
-//            override fun onClick(view: View, position: Int, data: Subjects?, viewType: Int) {
-//                toast("onClick---data===>${data?.title}")
-//                startActivity<WebActivity>(
-//                    if (position == 0) {
-//                        "url" to "http://debugtbs.qq.com"
-//                    } else {
-//                        "data" to ""
-//                    }
-//                )
-//            }
-//
-//            override fun onLongClick(view: View, position: Int, data: Subjects?, viewType: Int): Boolean {
-//                toast("onLongClick---data===>${data?.title}")
-//                return super.onLongClick(view, position, data, viewType)
-//            }
+//            toast("X5初始化---${WebConfig.hasX5()}")
+//            startActivity<HtmlPrinterActivity>()
+//            finish()
 //        }
 //    }
+
+        override fun initData(savedInstanceState: Bundle?) {
+        val factory = BeanDataSourceFactory()
+        val listing = Listing(
+            pagedList = factory.toLiveData(config),
+            networkState = Transformations.switchMap(factory.sourceLiveData) { it.networkState },
+            refreshState = Transformations.switchMap(factory.sourceLiveData) { it.initialLoad },
+            empty = Transformations.switchMap(factory.sourceLiveData) { it.empty },
+            refresh = { factory.sourceLiveData.value?.invalidate() },
+            retry = { factory.sourceLiveData.value?.retryAllFailed() },
+            exception = Transformations.switchMap(factory.sourceLiveData) { it.exception }
+        )
+        adapter = BeanAdapter {
+            listing.retry.invoke()
+        }
+        list.adapter = adapter
+        listing.pagedList.observe(this, Observer {
+            adapter.submitList(it)
+            Timber.d("adapter.currentList?.size===>${adapter.currentList?.size}")
+            adapter.currentList?.forEach { sub ->
+                Timber.d("adapter.currentList$sub")
+            }
+        })
+        listing.empty.observe(this, Observer {
+            toast("empty--$it")
+        })
+        listing.exception.observe(this, Observer {
+            toast("exception${it.message}")
+        })
+        listing.networkState.observe(this, Observer {
+            adapter.setNetworkState(it)
+        })
+        listing.refreshState.observe(this, Observer {
+            Timber.d("refreshState===$it")
+            swipe_refresh.isRefreshing = it == NetworkStatus.RUNNING
+        })
+        swipe_refresh.setOnRefreshListener {
+            Timber.d("refreshState===1111")
+            listing.refresh.invoke()
+        }
+        adapter.listener = object : SimplePagedListAdapter.OnViewClickListener<Subjects>() {
+            override fun onClick(view: View, position: Int, data: Subjects?, viewType: Int) {
+                toast("onClick---data===>${data?.title}")
+                startActivity<WebActivity>(
+                    if (position == 0) {
+                        "url" to "http://debugtbs.qq.com"
+                    } else {
+                        "data" to ""
+                    }
+                )
+            }
+
+            override fun onLongClick(view: View, position: Int, data: Subjects?, viewType: Int): Boolean {
+                toast("onLongClick---data===>${data?.title}")
+                return super.onLongClick(view, position, data, viewType)
+            }
+        }
+    }
 }
