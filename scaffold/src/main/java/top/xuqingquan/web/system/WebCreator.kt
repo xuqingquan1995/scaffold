06266ae1601d4a1f631342a78046f2643d4f6edb
许清泉 <xuqingquan1995@gmail.com>
2020-06-30 17:35:49 +0800

    优化部分导包

--- scaffold/src/main/java/top/xuqingquan/web/system/WebCreator.kt
+++ scaffold/src/main/java/top/xuqingquan/web/system/WebCreator.kt
@@ -1,12 +1,13 @@
 package top.xuqingquan.web.system
 
+import android.webkit.WebView
 import android.widget.FrameLayout
 import top.xuqingquan.web.nokernel.BaseIndicatorSpec
 import top.xuqingquan.web.nokernel.IWebIndicator
 
 interface WebCreator<T : BaseIndicatorSpec> : IWebIndicator<T> {
 
-    fun getWebView(): android.webkit.WebView?
+    fun getWebView(): WebView?
 
     fun getWebParentLayout(): FrameLayout
 
--- scaffold/src/main/java/top/xuqingquan/web/x5/WebCreator.kt
+++ scaffold/src/main/java/top/xuqingquan/web/x5/WebCreator.kt
@@ -1,12 +1,13 @@
 package top.xuqingquan.web.x5
 
 import android.widget.FrameLayout
+import com.tencent.smtt.sdk.WebView
 import top.xuqingquan.web.nokernel.BaseIndicatorSpec
 import top.xuqingquan.web.nokernel.IWebIndicator
 
 interface WebCreator<T : BaseIndicatorSpec> : IWebIndicator<T> {
 
-    fun getWebView(): com.tencent.smtt.sdk.WebView?
+    fun getWebView(): WebView?
 
     fun getWebParentLayout(): FrameLayout
 
