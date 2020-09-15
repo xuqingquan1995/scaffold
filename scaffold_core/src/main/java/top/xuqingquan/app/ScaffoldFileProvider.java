ec65beb2c8ed9f958f17821dd135a90e1b90c263
许清泉 <xuqingquan1995@gmail.com>
2020-09-16 01:01:10 +0800

    删除非必须

--- scaffold_core/src/main/java/top/xuqingquan/app/ScaffoldFileProvider.java
+++
@@ -1,8 +0,0 @@
-package top.xuqingquan.app;
-
-import android.support.v4.content.FileProvider;
-
-/**
- * Created by 许清泉 on 2019-09-14 23:11
- */
-public class ScaffoldFileProvider extends FileProvider {}
